6e3467abb14136465cf62af486d7d95c34a5d66d
B Karthik <bangikarthik7@gmail.com>
2025-01-23 23:47:10 +0530

    start: backend started

--- backend/src/utils/get-env.ts
+++ backend/src/utils/get-env.ts
@@ -1,10 +1,10 @@
 export const getEnv = (key: string, defaultValue: string = ""): string => {
   const value = process.env[key];
-  if (!value) {
+  if (value === undefined) {
     if (defaultValue) {
       return defaultValue;
     }
-    throw new Error(`Missing required environment variable: ${key}`);
+    throw new Error(`Enviroment variable ${key} is not set`);
   }
   return value;
 };
